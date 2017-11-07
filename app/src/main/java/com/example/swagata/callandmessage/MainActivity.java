8cc5203e5d45b99a1d7bde023e88fb4babd8826f
swagata <swagatadas.wb@gmail.com>
2017-11-07 23:33:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/swagata/callandmessage/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.swagata.callandmessage;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telecom.Call;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton button = (ImageButton) findViewById(R.id.send_sms);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", "9163630746", null));
+                intent.putExtra("sms_body", "Hello dear...");
+                startActivity(intent);
+            }
+        });
+
+
+        ImageButton button1 = (ImageButton) findViewById(R.id.call);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                Intent intent = callIntent.setData(Uri.parse("9163630746"));
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
+
+    }
+}
+
